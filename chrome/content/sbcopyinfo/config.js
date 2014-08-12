4c20a88e52c98aad31e866f657c1d08af509be4e
Danny Lin <danny0838@gmail.com>
2014-08-12 10:29:15 +0800

    Extract .jar

---
+++ chrome/content/sbcopyinfo/config.js
@@ -0,0 +1,33 @@
+var sbCopyInfoConfig = {
+
+	get TEXTBOX() { return document.getElementById("sbCopyInfoTemplate"); },
+
+	init : function()
+	{
+		this.TEXTBOX.value = nsPreferences.copyUnicharPref("scrapbook.copyPageInfo", "%TITLE%\n%SOURCE%\n");
+		this.TEXTBOX.focus();
+	},
+
+	done : function()
+	{
+		nsPreferences.setUnicharPref("scrapbook.copyPageInfo", this.TEXTBOX.value);
+	},
+
+	setPreset : function(flag)
+	{
+		var val;
+		switch ( flag )
+		{
+			case "TS"   : val = "%TITLE%\n%SOURCE%\n"; break;
+			case "TL"   : val = "%TITLE%\n%LOCAL%\n"; break;
+			case "TS+"  : val = "<a href=\"%SOURCE%\">%TITLE%</a>"; break;
+			case "TL+"  : val = "<a href=\"%LOCAL%\">%TITLE%</a>"; break;
+			case "TSS+" : val = "%TITLE%\n<a href=\"%SOURCE%\">%SOURCE%</a>\n"; break;
+			case "TLL+" : val = "%TITLE%\n<a href=\"%LOCAL%\">%LOCAL%</a>\n"; break;
+		}
+		this.TEXTBOX.value = val;
+	},
+
+};
+
+
